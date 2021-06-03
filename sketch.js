14e323db1e9ba59b110378cd1c2d0ebe239c8917
Roshan <crjacob2007@gmail.com>
2021-06-03 13:17:12 +0530

    Commiting first-time!

---
+++ sketch.js
@@ -0,0 +1,7 @@
+function clicket() {
+  document.getElementById("clickable").addEventListener("click", () => {
+    alert(
+      "You have clicked a button! Couldn't resist? Well, this won't lead you to a virus, but be sure to think before clicking something on a webpage!"
+    );
+  });
+}
